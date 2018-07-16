0b17e000d88392899a91bfea37b4fbe886a8029b
partholon <zhangkj09@gmail.com>
2018-07-16 17:18:56 +0800

    重写八联通函数。

--- skin/源.cpp
+++ skin/源.cpp
@@ -41,37 +41,34 @@ public:
 	}
 };
 
-void Seed_Filling(const cv::Mat& binImg, cv::Mat& labelImg, int& labelNum,
-	int(&ymin)[20], int(&ymax)[20], int(&xmin)[20], int(&xmax)[20])   //种子填充法  
-{
+//八向联通确定边界
+void EightConnections(const cv::Mat& binImg, int& labelNum,
+	vector<int>& ymin, vector<int>& ymax, vector<int>& xmin, vector<int>& xmax) {
+
 	if (binImg.empty() || binImg.type() != CV_8UC1)
 		return;
 
-	labelImg.release();
-	binImg.convertTo(labelImg, CV_32FC1);
+	Mat labelImg;
+	binImg.convertTo(labelImg, CV_32SC1);
+
 	int label = 2;
 	int rows = binImg.rows - 1;
 	int cols = binImg.cols - 1;
-	for (int i = 1; i < rows - 1; i++)
-	{
+	for (int i = 1; i < rows - 1; i++) {
 		int* data = labelImg.ptr<int>(i);
-		for (int j = 1; j < cols - 1; j++)
-		{
-			if (data[j] == 1)
-			{
+		for (int j = 1; j < cols - 1; j++) {
+			if (*(data + j) == 1) {
 				stack<pair<int, int>> neighborPixels;
-				neighborPixels.push(pair<int, int>(j, i));     // 像素位置: <j,i>  
-				ymin[label] = i;
-				ymax[label] = i;
-				xmin[label] = j;
-				xmax[label] = j;
-				while (!neighborPixels.empty())
-				{
-					std::pair<int, int> curPixel = neighborPixels.top(); //如果与上一行中一个团有重合区域，则将上一行的那个团的标号赋给它  
+				neighborPixels.push(pair<int, int>(j, i)); 
+				ymin.push_back(i);
+				ymax.push_back(i);
+				xmin.push_back(j);
+				xmax.push_back(j);
+				while (!neighborPixels.empty()) {
+					pair<int, int> curPixel = neighborPixels.top(); 
 					int curX = curPixel.first;
 					int curY = curPixel.second;
 					labelImg.at<int>(curY, curX) = label;
-
 					neighborPixels.pop();
 
 					// 1是二值图像中的边界
@@ -85,56 +82,56 @@ void Seed_Filling(const cv::Mat& binImg, cv::Mat& labelImg, int& labelNum,
 						if (labelImg.at<int>(curY + 1, curX) == 1)						//下
 						{
 							neighborPixels.push(std::pair<int, int>(curX, curY + 1));
-							if ((curY + 1)>ymax[label])
-								ymax[label] = curY + 1;
+							if ((curY + 1)>ymax[label - 2])
+								ymax[label - 2] = curY + 1;
 						}
 						if (labelImg.at<int>(curY, curX - 1) == 1)						//左
 						{
 							neighborPixels.push(std::pair<int, int>(curX - 1, curY));
-							if ((curX - 1)<xmin[label])
-								xmin[label] = curX - 1;
+							if ((curX - 1)<xmin[label - 2])
+								xmin[label - 2] = curX - 1;
 						}
 						if (labelImg.at<int>(curY, curX + 1) == 1)						//右
 						{
 							neighborPixels.push(std::pair<int, int>(curX + 1, curY));
-							if ((curX + 1)>xmax[label])
-								xmax[label] = curX + 1;
+							if ((curX + 1)>xmax[label - 2])
+								xmax[label - 2] = curX + 1;
 						}
 						if (labelImg.at<int>(curY - 1, curX - 1) == 1)					//左上
 						{
 							neighborPixels.push(std::pair<int, int>(curX - 1, curY - 1));
-							//ymin[label] = curY - 1;
-							if ((curX - 1)<xmin[label])
-								xmin[label] = curX - 1;
+							//ymin[label-2] = curY - 1;
+							if ((curX - 1)<xmin[label - 2])
+								xmin[label - 2] = curX - 1;
 						}
 						if (labelImg.at<int>(curY + 1, curX + 1) == 1)					//右下
 						{
 							neighborPixels.push(std::pair<int, int>(curX + 1, curY + 1));
-							if ((curY + 1)>ymax[label])
-								ymax[label] = curY + 1;
-							if ((curX + 1)>xmax[label])
-								xmax[label] = curX + 1;
+							if ((curY + 1)>ymax[label - 2])
+								ymax[label - 2] = curY + 1;
+							if ((curX + 1)>xmax[label - 2])
+								xmax[label - 2] = curX + 1;
 
 						}
 						if (labelImg.at<int>(curY + 1, curX - 1) == 1)					//左下
 						{
 							neighborPixels.push(std::pair<int, int>(curX - 1, curY + 1));
-							if ((curY + 1)>ymax[label])
-								ymax[label] = curY + 1;
-							if ((curX - 1)<xmin[label])
-								xmin[label] = curX - 1;
+							if ((curY + 1)>ymax[label - 2])
+								ymax[label - 2] = curY + 1;
+							if ((curX - 1)<xmin[label - 2])
+								xmin[label - 2] = curX - 1;
 						}
 						if (labelImg.at<int>(curY - 1, curX + 1) == 1)					//右上
 						{
 							neighborPixels.push(std::pair<int, int>(curX + 1, curY - 1));
-							//ymin[label] = curY - 1;
-							if ((curX + 1)>xmax[label])
-								xmax[label] = curX + 1;
+							//ymin[label-2] = curY - 1;
+							if ((curX + 1)>xmax[label - 2])
+								xmax[label - 2] = curX + 1;
 
 						}
 					}
 				}
-				label++;  // 没有重复的团，开始新的标签 
+				label++; 
 			}
 		}
 	}
@@ -143,17 +140,15 @@ void Seed_Filling(const cv::Mat& binImg, cv::Mat& labelImg, int& labelNum,
 
 int main() {
 	vector<Mat> channels;
-	Mat origin,bin,tmp;
+	Mat origin, bin, tmp;
 	Mat Y, Cr, Cb;
 
 	Mat tmpl = imread("bwz.jpg", CV_8UC1);
-	origin = imread("2.jpg");
-	//resize(origin, origin, Size(0,0), 0.15, 0.15, INTER_AREA);
-	//namedWindow("origin");
+	origin = imread("low.jpg");
+	resize(origin, origin, Size(0, 0), 0.15, 0.15, INTER_AREA);
 	//imshow("origin", origin);
 
 	//转换颜色空间
-	//bin.create(resizeimg.rows, resizeimg.cols, CV_8UC1); //单通道灰度图
 	cvtColor(origin, bin, CV_BGR2GRAY);
 	origin.copyTo(tmp);
 	cvtColor(tmp, tmp, CV_BGR2YCrCb);
@@ -173,7 +168,7 @@ int main() {
 				pointbin[j] = 0;
 		}
 	}
-	
+
 	dilate(bin, bin, Mat());
 
 	//watershed algorithm
@@ -192,14 +187,14 @@ int main() {
 	segmenter.process(origin);
 	Mat waterShed;
 	waterShed = segmenter.getWatersheds();
-	//get area
+	//Get area
 	threshold(waterShed, waterShed, 1, 1, THRESH_BINARY_INV);
 	//imshow("watershed", waterShed);
-	
-	Mat labelImg;
-	int label, ymin[20], ymax[20], xmin[20], xmax[20];
-	Seed_Filling(waterShed, labelImg, label, ymin, ymax, xmin, xmax);
-	
+
+	int label;
+	vector<int>ymin, ymax, xmin, xmax;
+	EightConnections(waterShed, label, ymin, ymax, xmin, xmax);
+
 	////根据标记，对每块候选区就行缩放，并与模板比较
 	//Size dsize = Size(tmpl.cols, tmpl.rows);
 	//float simi[20];
@@ -218,26 +213,26 @@ int main() {
 	//		imshow("temp", bin);
 	//	}
 	//}
-	
+
 	//统计一下区域中的肤色区域比例  --- 并没有排除噪点
 	float fuseratio[20];
 	for (int k = 0; k < label; k++)
 	{
-		fuseratio[k + 2] = 1;
-		if (((xmax[k + 2] - xmin[k + 2])>50) && ((ymax[k + 2] - ymin[k + 2]) > 50))
+		fuseratio[k] = 1;
+		if (((xmax[k] - xmin[k])>50) && ((ymax[k] - ymin[k]) > 50))
 		{
 			int fusepoint = 0;
-			for (int j = ymin[k + 2]; j < ymax[k + 2]; j++)
+			for (int j = ymin[k]; j < ymax[k]; j++)
 			{
 				uchar* current = bin.ptr< uchar>(j);
-				for (int i = xmin[k + 2]; i < xmax[k + 2]; i++)
+				for (int i = xmin[k]; i < xmax[k]; i++)
 				{
 					if (current[i] == 255)
 						fusepoint += 1;
 				}
 			}
-			fuseratio[k + 2] = float(fusepoint) / ((xmax[k + 2] - xmin[k + 2])*(ymax[k + 2] - ymin[k + 2]));
-			//cout << fuseratio[k + 2] << endl;
+			fuseratio[k] = float(fusepoint) / ((xmax[k] - xmin[k])*(ymax[k] - ymin[k]));
+			//cout << fuseratio[k] << endl;
 		}
 	}
 
@@ -245,9 +240,9 @@ int main() {
 	for (int i = 0; i < label; i++)
 	{
 		if (/*(simi[i]<0.02) &&*/ (fuseratio[i] < 0.65))
-			rectangle(origin, Point(xmin[i + 1], ymin[i + 2]), Point(xmax[i + 2], ymax[i + 2]), Scalar::all(255), 2, 8, 0);
+			rectangle(origin, Point(xmin[i], ymin[i]), Point(xmax[i], ymax[i]), Scalar(0,0,255), 2, 8, 0);
 	}
-	
+
 	//imshow("trans", bin);
 	imshow("result", origin);
 	waitKey();
